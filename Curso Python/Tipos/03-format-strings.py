f31d0a1ab2ceaf0460d843875eab5490ea5dcbc6
Cristian Laurentiu Sindila <cl.sindila@gmail.com>
2023-04-25 17:28:34 +0200

    CPython 03

---
+++ Curso Python/Tipos/03-format-strings.py
@@ -0,0 +1,7 @@
+nombre = "Cristian"
+apellido = "Sindila"
+# nombre_completo = nombre + " " + apellido
+nombre_completo = f"{nombre[0]} {apellido}"
+otro_nombre = f"{nombre[:4]} {2+2}"
+print(nombre_completo)
+print(otro_nombre)
